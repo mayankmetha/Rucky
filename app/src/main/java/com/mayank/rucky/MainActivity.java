804c588154d31c818e3773b77f81bc3f899de072
mayankmetha <mayankmethad@gmail.com>
2018-01-13 16:05:15 +0530

    init

---
+++ app/src/main/java/com/mayank/rucky/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mayank.rucky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
