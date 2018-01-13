3631c31850cd18fe9cd2042ffbb03a4abadf04ec
mayankmetha <mayankmethad@gmail.com>
2018-01-13 17:18:54 +0530

    fix warnings

--- app/src/main/java/com/mayank/rucky/MainActivity.java
+++ app/src/main/java/com/mayank/rucky/MainActivity.java
@@ -3,8 +3,6 @@ package com.mayank.rucky;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         try {
-            Process su = Runtime.getRuntime().exec("sh -c su");
+            Runtime.getRuntime().exec("sh -c su");
         } catch (Exception e) {
             e.printStackTrace();
         }
--- app/src/main/java/com/mayank/rucky/SecretCodeReceiver.java
+++ app/src/main/java/com/mayank/rucky/SecretCodeReceiver.java
@@ -4,11 +4,13 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Objects;
+
 public class SecretCodeReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent)throws NullPointerException {
-        if(intent.getAction().equals("android.provider.Telephony.SECRET_CODE")) {
+    public void onReceive(Context context, Intent intent) {
+        if(Objects.equals(intent.getAction(), "android.provider.Telephony.SECRET_CODE")) {
             Intent i = new Intent(context, MainActivity.class);
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
