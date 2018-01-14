cd2cef489794b03355ed8d3348c403f27a3d5e5f
mayankmetha <mayankmethad@gmail.com>
2018-01-14 23:49:10 +0530

    Fix setting ui

--- app/src/main/java/com/mayank/rucky/SettingsActivity.java
+++ app/src/main/java/com/mayank/rucky/SettingsActivity.java
@@ -1,10 +1,12 @@
 package com.mayank.rucky;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.SwitchPreferenceCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,15 +16,21 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = findViewById(R.id.toolbarSettings);
         setSupportActionBar(toolbar);
+        getSupportFragmentManager().beginTransaction().add(R.id.SettingsPage, new SettingsFragment()).commit();
+        //SharedPreferences.Editor editor = getSharedPreferences("myPref",MODE_PRIVATE).edit();
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if(id == android.R.id.home) {
-            //NavUtils.navigateUpFromSameTask(this);
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
