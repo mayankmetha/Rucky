685d53575d3986ce2e1f100343c22bdfc1559898
mayankmetha <mayankmethad@gmail.com>
2018-01-15 22:59:03 +0530

    Fix Settings

--- app/src/main/java/com/mayank/rucky/MainActivity.java
+++ app/src/main/java/com/mayank/rucky/MainActivity.java
@@ -1,17 +1,25 @@
 package com.mayank.rucky;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Boolean didThemeChange = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final SharedPreferences settings = getSharedPreferences(SettingsActivity.PREF_SETTINGS, MODE_PRIVATE);
+        SettingsActivity.darkTheme = settings.getBoolean(SettingsActivity.PREF_SETTINGS_DARK_THEME,false);
+        theme();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbarMain);
         setSupportActionBar(toolbar);
@@ -39,5 +47,25 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        theme();
+        if(didThemeChange) {
+            didThemeChange = false;
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+    }
+
+    void theme() {
+        if(SettingsActivity.darkTheme) {
+            setTheme(R.style.AppThemeDark);
+        } else {
+            setTheme(R.style.AppThemeLight);
+        }
     }
 }
--- app/src/main/java/com/mayank/rucky/SettingsActivity.java
+++ app/src/main/java/com/mayank/rucky/SettingsActivity.java
@@ -1,23 +1,52 @@
 package com.mayank.rucky;
 
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.SwitchPreferenceCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String PREF_SETTINGS = "settings";
+    public static final String PREF_SETTINGS_DARK_THEME = "darkTheme";
+    public static Boolean darkTheme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final SharedPreferences settings = getSharedPreferences(PREF_SETTINGS, MODE_PRIVATE);
+        darkTheme = settings.getBoolean(PREF_SETTINGS_DARK_THEME,false);
+        theme();
         setContentView(R.layout.activity_settings);
         Toolbar toolbar = findViewById(R.id.toolbarSettings);
         setSupportActionBar(toolbar);
-        getSupportFragmentManager().beginTransaction().add(R.id.SettingsPage, new SettingsFragment()).commit();
-        //SharedPreferences.Editor editor = getSharedPreferences("myPref",MODE_PRIVATE).edit();
+        Switch themeSwitch = findViewById(R.id.darkThemeSwitch);
+        themeSwitch.setChecked(darkTheme);
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                darkTheme = isChecked;
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putBoolean(PREF_SETTINGS_DARK_THEME, darkTheme).apply();
+                MainActivity.didThemeChange = true;
+            }
+        });
+        String version = "Version: ";
+        try {
+            PackageInfo pInfo = this.getPackageManager().getPackageInfo(getPackageName(),0);
+            version = version+pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        TextView versionText = findViewById(R.id.Version);
+        versionText.setText(version);
     }
 
     @Override
@@ -32,5 +61,14 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     public void onResume() {
         super.onResume();
+        theme();
+    }
+
+    void theme() {
+        if(darkTheme) {
+            setTheme(R.style.AppThemeDark);
+        } else {
+            setTheme(R.style.AppThemeLight);
+        }
     }
 }
