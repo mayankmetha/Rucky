7b0166853ae4cc44d7c2a80fbf3b8c5795a0e26c
mayankmetha <mayankmethad@gmail.com>
2020-04-12 17:09:52 +0530

    Updated pi socket for mDNS

--- app/src/main/java/com/mayank/rucky/wifiSocket.java
+++ app/src/main/java/com/mayank/rucky/wifiSocket.java
@@ -11,8 +11,8 @@ public class wifiSocket implements Runnable{
     private String ip;
     private ArrayList<String> cmds;
 
-    wifiSocket(ArrayList<String> cmds) {
-        this.ip = "192.168.1.1";
+    wifiSocket(ArrayList<String> cmds, String ip) {
+        this.ip = ip;
         this.cmds = new ArrayList<>();
         this.cmds.addAll(cmds);
     }
@@ -21,21 +21,21 @@ public class wifiSocket implements Runnable{
         try {
             int port = 5000;
             Socket socket = new Socket(ip, port);
-            MainActivity.piConnected = true;
-            while(MainActivity.piConnected) {
+            MainActivity.piSocketConnected = true;
+            while(MainActivity.piSocketConnected) {
                 try {
                     PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
                     for(int i = 0; i < cmds.size(); i++) {
                         out.println(cmds.get(i));
                     }
-                    MainActivity.piConnected = false;
+                    MainActivity.piSocketConnected = false;
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
             socket.close();
         } catch (Exception e) {
-            MainActivity.piConnected = false;
+            MainActivity.piSocketConnected = false;
             e.printStackTrace();
         }
     }
