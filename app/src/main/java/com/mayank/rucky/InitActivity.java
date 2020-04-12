8bd18096d8bc4d2b9074faf925c631da1a4b108e
mayankmetha <mayankmethad@gmail.com>
2020-04-12 22:51:37 +0530

    New one time init screen

--- app/src/main/java/com/mayank/rucky/InitActivity.java
+++ app/src/main/java/com/mayank/rucky/InitActivity.java
@@ -55,9 +55,7 @@ public class InitActivity extends AppCompatActivity {
         layouts = new int[]{
                 R.layout.slide1,
                 R.layout.slide2,
-                R.layout.slide3,
-                R.layout.slide4,
-                R.layout.slide5
+                R.layout.slide3
         };
 
         addBottomDots(0);
