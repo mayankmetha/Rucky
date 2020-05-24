49a76c14e84e87ff473bc42830785f69d82ddfcb
mayankmetha <mayankmethad@gmail.com>
2020-05-24 22:19:38 +0530

    Fix for older navigation bar.

--- app/src/main/java/com/mayank/rucky/InitActivity.java
+++ app/src/main/java/com/mayank/rucky/InitActivity.java
@@ -86,7 +86,9 @@ public class InitActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
@@ -146,12 +148,10 @@ public class InitActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-        }
+        public void onPageScrolled(int position, float arg1, int arg2) {}
 
         @Override
-        public void onPageScrollStateChanged(int arg0) {
-        }
+        public void onPageScrollStateChanged(int arg0) {}
     };
 
     public class MyViewPagerAdapter extends PagerAdapter {
