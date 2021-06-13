dcd2e4d113aa12452b90296f602e7a47cdd428a6
mayankmetha <mayankmethad@gmail.com>
2021-06-13 10:28:34 +0530

    Fixed Readme

--- app/gencon.sh
+++ app/gencon.sh
@@ -1,26 +1,18 @@
-while getopts b:a:n flag
-do
-    case "${flag}" in
-        a) minSdkVersion=${OPTARG};;
-        b) buildType=${OPTARG};;
-        n) lastBuild=${OPTARG};;
-    esac
-done
-if [ "$buildType" = "release" ]; then
+if [ "$1" = "release" ]; then
     openssl sha512 ./release/rucky.apk | cut -d"=" -f2 | cut -d" " -f2 > ./release/rucky.sha512
-    echo "$minSdkVersion" > ./release/rucky.cfg
+    echo "$2" > ./release/rucky.cfg
     echo "$(($(git rev-list HEAD --count master)+1))" >> ./nightly/rucky.cfg
-elif [ "$buildType" = "nightly" ]; then
-    read -ap "Commit Msg:" commitMsg
+elif [ "$1" = "nightly" ]; then
+    read -p "Commit Msg:" commitMsg
     cp ./nightly/rucky-nightly.apk ../nightly/rucky-nightly.apk
     openssl sha512 ./nightly/rucky-nightly.apk | cut -d"=" -f2 | cut -d" " -f2 > ../nightly/rucky.cfg
-    echo "$minSdkVersion" > ../nightly/rucky.cfg
+    echo "$2" > ../nightly/rucky.cfg
     nextCommitCount=$(($(git rev-list HEAD --count master)+1))
     echo "$(($(git rev-list HEAD --count master)+1))" >> ../nightly/rucky.cfg
-    diffCommitCount=$(($(git rev-list HEAD --count master)-$lastBuild))
+    diffCommitCount=$(($(git rev-list HEAD --count master)-$3))
     git log --oneline -$((diffCommitCount)) -s --format='• %s' | uniq -iu | tail -r > ../nightly/Changelog
-    echo "• $($commitMsg)" >> ../nightly/Changelog
+    echo "• $commitMsg" >> ../nightly/Changelog
     git add -f ../nightly/rucky-nightly.apk ../nightly/rucky.cfg ../nightly/rucky.sha512 ../nightly/Changelog
-    git commit -m "`$commitMsg`"
+    git commit -m "$commitMsg"
     git push
 fi
