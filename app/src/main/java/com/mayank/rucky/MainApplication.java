e5320e71d0d9a8be66c3dbf3a4d67e76693de758
mayankmetha <mayankmethad@gmail.com>
2022-01-09 13:28:29 +0530

    Added static monet theme

--- app/src/main/java/com/mayank/rucky/MainApplication.java
+++ app/src/main/java/com/mayank/rucky/MainApplication.java
@@ -3,10 +3,16 @@ package com.mayank.rucky;
 import android.app.Application;
 import android.content.res.Configuration;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.google.android.material.color.DynamicColors;
+import com.mayank.rucky.utils.Config;
+
 public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this, R.style.Theme_Rucky_Monet);
     }
 
     @Override
